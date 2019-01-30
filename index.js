8a8cc092f3554377a2ac1869e54d9cdac914ba2b
Hernán Giraldo <hernan.giraldo.giraldo@gmail.com>
2019-01-29 21:18:13 -0400

    Promesas

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require('./src/call');
+
+// console.log(call.sync('Hernán'));
+
+// call.withCallback(
+//     'Hernán',
+//     (z) => console.log(z)
+// );
+
+(async () => {
+    console.log(await call.withPromise('Hernán'));
+})();
