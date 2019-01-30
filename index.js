447c40bb25a8b2e2d9c71db20bae5f14e867189b
Hernán Giraldo <hernan.giraldo.giraldo@gmail.com>
2019-01-29 22:32:33 -0400

    Manejo de errores

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const errors = require('./src/errors');
+const { sync, errorFirstCallbackWrong, errorFirstCallback } = require('./src/handling');
+
+try {
+    errors.standardErr.range();
+} catch(e) {
+    console.log('Ha ocurrido un error')
+}
+
+sync();
+errorFirstCallbackWrong();
+errorFirstCallback();
