c80e785ba8ba740a10d54a82bdbbdbf3d776c60a
Hernán Giraldo <hernan.giraldo.giraldo@gmail.com>
2019-01-29 22:59:36 -0400

    Eventos

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const greet = require('./src/greet');
+
+greet.emit('clap');
+
+greet.emit('shout', 'Hernán');
+
+greet.emit(
+    'call',
+    'Hernán',
+    (n) => console.log(n)
+)
