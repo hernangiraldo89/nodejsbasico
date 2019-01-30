64705e4a8b70d2afde18874d0ff26a5986cebd6f
Hernán Giraldo <hernan.giraldo.giraldo@gmail.com>
2019-01-29 22:09:10 -0400

    Child process

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const cp = require('child_process');
+
+const execCommand = (command) => {
+    cp.exec(command, (err, stdout, stderr) => {
+        if (err) throw err;
+        if (stdout) console.log(stdout);
+        if (stderr) console.log(stderr);
+    });
+}
+
+execCommand('ls');
+execCommand('node ./src/script.js --base=5')
