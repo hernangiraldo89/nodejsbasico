9130a60f9db33bf9bf75066cd1cc969edc73ed88
Hernán Giraldo <hernan.giraldo.giraldo@gmail.com>
2019-01-29 21:26:55 -0400

    Async/Await

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require('./src/call');
+
+// call.greet('Hernán')
+
+(async () => {
+    console.log(await call.withPromise('Hernán', 'Giraldo'));
+})();
